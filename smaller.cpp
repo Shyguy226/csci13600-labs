b88c22015ed43c0b143df388f3ef1aeb627537a0
Shyguy226 <40038319+Shyguy226@users.noreply.github.com>
2018-08-30 23:24:28 -0400

    Update smaller.cpp

--- smaller.cpp
+++ smaller.cpp
@@ -3,22 +3,24 @@ Author: Andrew Giannico
 Course: CSCI-133
 Instructor: Mr. Zamanksy
 Assignment: Lab1A
+
+This program will receive two numbers and print the smaller of the two.
 */
 #include <iostream>
 using namespace std;
 
 int main() {
-cout << “Enter the first number: “ << endl;
+cout << "Enter the first number: " << endl;
 int first = 0;
 cin >> first;
-cout << “Enter the second number: “ << endl;
+cout << "Enter the second number: " << endl;
 int second = 0;
 cin >> second;
 if (first > second){
-cout << “The smaller of the two is “ << second << endl;
+  cout << "The smaller of the two is " << second << endl;
 }
 else {
-cout << “The smaller of the two is “ << first << endl;
+  cout << "The smaller of the two is " << first << endl;
 }
 return 0;
 }
