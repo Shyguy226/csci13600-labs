4b31ef0b7b99cb788eb458d85da3d7de8824d838
Shyguy226 <40038319+Shyguy226@users.noreply.github.com>
2018-08-30 23:32:34 -0400

    Update smaller3.cpp

--- smaller3.cpp
+++ smaller3.cpp
@@ -3,36 +3,28 @@ Author: Andrew Giannico
 Course: CSCI-133
 Instructor: Mr. Zamanksy
 Assignment: Lab1B
-
-This program accepts three numbers and prints the smallest of the three.
 */
 #include <iostream>
 using namespace std;
 
 int main() {
-cout << “Enter the first number: “ << endl;
+cout << "Enter the first number: " << endl;
 int first = 0;
 cin >> first;
-cout << “Enter the second number: “ << endl;
+cout << "Enter the second number: " << endl;
 int second = 0;
 cin >> second;
-cout << “Enter the third number: “ << endl;
+cout << "Enter the third number: " << endl;
 int third = 0;
-if(first>second){
-  if(second<third){
-    cout << "The smaller of the three is " << second << endl;
-    }
-  else {
-    cout << "The smaller of the three is " << third << endl;
-    }
-  }
+cin >> third;
+if(first > second && second > third){
+  cout << "The smaller of the three is " << third << endl;
+}
+else if(first > second && second < third){
+  cout << "The smaller of the three is " << second << endl;
+}
 else {
-  if(first>third){
-    cout << "The smaller of the three is " << third << endl;
-    }
-  else{
-    cout << "The smaller of the three is " << first << endl;
-    }
-  }
+  cout << "The smaller of the three is " << first << endl;
+}
 return 0;
 }
