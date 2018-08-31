0aa0111df49596cd6609323c5cfcec002ca825e3
Shyguy226 <40038319+Shyguy226@users.noreply.github.com>
2018-08-30 23:42:58 -0400

    Update smaller3.cpp

--- smaller3.cpp
+++ smaller3.cpp
@@ -17,20 +17,29 @@ cin >> second;
 cout << "Enter the third number: " << endl;
 int third = 0;
 cin >> third;
-if(first > second && second > third){
-  cout << "The smaller of the three is " << third << endl;
+if (first > second && first > third){
+  if (second < third){
+    cout << "The smaller of the three is " << second << endl;
+  }
+  else {
+    cout << "The smaller of the three is " << third << endl;
+  }
 }
-else if(first > second && second < third){
-  cout << "The smaller of the three is " << second << endl;
-}
-else if (first < second && second > third){
-  cout << "The smaller of the three is " << first << endl;
-}
-else if (first > third){
-  cout << "The smaller of the three is " << third << endl;
+else if (second > first && second > third){
+  if (first < third){
+    cout << "The smaller of the three is " << first << endl;
+  }
+  else {
+    cout << "The smaller of the three is " << third << endl;
+  }
 }
 else {
-  cout << "The smaller of the three is " << first << endl;
+  if (first < second){
+    cout << "The smaller of the three is " << first << endl;
+  }
+  else {
+    cout << "The smaller of the three is " << second << endl;
+  }
 }
 return 0;
 }
