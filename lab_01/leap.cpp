730c05f88366f3c26ae527c6012efca20dace2ef
Shyguy226 <40038319+Shyguy226@users.noreply.github.com>
2018-09-05 09:59:04 -0400

    Create leap.cpp

---
+++ lab_01/leap.cpp
@@ -0,0 +1,27 @@
+/*
+Author: Andrew Giannico
+Course: CSCI-133
+Instructor: Mr. Zamanksy
+Assignment: Lab1C
+This program will tell the user if the year they inputted is a leap year.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "Please enter a year: " << endl;
+int year = 0;
+cin >> year;
+if (year%4==0){
+  if(year%100==0 && year % 400 != 0){
+    cout << "Common year" << endl;
+    }
+  else {
+    cout << "Leap year" << endl;
+    }
+  }
+else {
+  cout << "Common year" << endl;
+  }
+}
+//test
