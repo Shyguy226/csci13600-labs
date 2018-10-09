67ef861881488d939f1cb7fe96dc7b09c8d8b9d8
Andrew Giannico <40038319+Shyguy226@users.noreply.github.com>
2018-10-09 12:17:19 -0400

    Create funcs.h

---
+++ lab_03/funcs.h
@@ -0,0 +1,6 @@
+#pragma once
+
+std::string minmax(std::string file_name);
+std::string compare (std::string date1, std::string date2, std::string file_name);
+std::string reverse_order (std::string date1, std::string date2, std::string file_name);
+double find_storage(std::string date, std::string file_name);
